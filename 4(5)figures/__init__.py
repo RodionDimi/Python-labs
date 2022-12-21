54e5c6ee597538bf866e8e07538286a2a28ab569
RodionDimi <113112092+RodionDimi@users.noreply.github.com>
2022-12-21 21:34:45 +0300

    __init__.py

--- 4(5)figures/__init__.py
+++ 4(5)figures/__init__.py
@@ -1,6 +1,6 @@
-from .circle import circle_area, circle_perimeter
-from .triangle import triangle_area, triangle_perimeter
-from .square import square_area, square_perimeter
+from figures import circle_area, circle_perimeter
+from figures import triangle_area, triangle_perimeter
+from figures import square_area, square_perimeter
 
 __all__ = (
     'circle_area',
