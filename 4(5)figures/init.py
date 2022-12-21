ec3f8dd318b998d77460da7b040bab795d083f85
RodionDimi <113112092+RodionDimi@users.noreply.github.com>
2022-12-21 21:33:31 +0300

    Update init.py

--- 4(5)figures/init.py
+++ 4(5)figures/init.py
@@ -1,3 +1,3 @@
-from .circle import circle_area, circle_perimeter
-from .triangle import triangle_area, triangle_perimeter
-from .square import square_area, square_perimeter
+from figures import circle_area, circle_perimeter
+from figures import triangle_area, triangle_perimeter
+from figures import square_area, square_perimeter
