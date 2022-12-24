2efe49bfc994c251d8ea62841ac2fe2cf7803117
RodionDimi <113112092+RodionDimi@users.noreply.github.com>
2022-12-24 17:48:28 +0300

    bot.py

--- bottg/bot.py
+++ bottg/bot.py
@@ -1,32 +1,36 @@
 import telebot
-import requests
+import random
+from telebot import types
 
-bot = telebot.TeleBot('5955607703:AAHOZ0AYb-tLB9B6V2uCX9qvls9inHZzpls')
+f = open('data/facts.txt', 'r', encoding='UTF-8')
+facts = f.read().split('\n')
+f.close()
 
+f = open('data/thinks.txt', 'r', encoding='UTF-8')
+thinks  = f.read().split('\n')
+f.close()
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id,'Привет. Напиши /help, чтобы узнать мои команды')
+bot = telebot.TeleBot('Здесь твой токен, полученный от @botfather')
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id,'Чтобы получить фото, напиши /starts')
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+        
+        markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1=types.KeyboardButton("Факт")
+        item2=types.KeyboardButton("Поговорка")
+        markup.add(item1)
+        markup.add(item2)
+        bot.send_message(m.chat.id, 'Нажми: \nФакт для получения интересного факта\nПоговорка — для получения мудрой цитаты ',  reply_markup=markup)
 
-@bot.message_handler(commands=['starts'])
-def starts(message):
-    r = requests.get('http://thecatapi.com/api/images/get?format=src')
-    url = r.url
-    bot.send_photo(message.chat.id, url)
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    
+    if message.text.strip() == 'Факт' :
+            answer = random.choice(facts)
+    
+    elif message.text.strip() == 'Поговорка':
+            answer = random.choice(thinks)
+    
+    bot.send_message(message.chat.id, answer)
 
-
-
-@bot.message_handler()
-def get_user_text(message):
-    if message.text == "Привет":
-        bot.send_message(message.chat.id," /help")
-    else:
-        bot.send_message(message.chat.id,"Напиши /help")
-
-
-
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)
